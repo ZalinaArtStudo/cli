39fb85ed94f92d934648f5751a07ec502e92da66
Ariel Caplan <ariel.caplan@shopify.com>
2022-11-12 20:34:51 +0200

    Test fixups

--- packages/app/src/cli/services/dev/select-app.test.ts
+++ packages/app/src/cli/services/dev/select-app.test.ts
@@ -120,7 +120,7 @@ describe('selectOrCreateApp', () => {
 
     // Then
     expect(got).toEqual(APP1)
-    expect(selectAppPrompt).toHaveBeenCalledWith([APP1, APP2, ORG1.id, 'token'])
+    expect(selectAppPrompt).toHaveBeenCalledWith([APP1, APP2], ORG1.id, 'token')
   })
 
   it('prompts user to select if cachedApiKey is invalid and chooses to select', async () => {
@@ -135,7 +135,7 @@ describe('selectOrCreateApp', () => {
 
     // Then
     expect(got).toEqual(APP1)
-    expect(selectAppPrompt).toHaveBeenCalledWith([APP1, APP2])
+    expect(selectAppPrompt).toHaveBeenCalledWith([APP1, APP2], ORG1.id, 'token')
   })
 
   it('prompts user to create if chooses to create', async () => {
